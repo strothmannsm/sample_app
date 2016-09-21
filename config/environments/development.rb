219927b1fdf446c9f6d8e216c83b615eb7c31dc2
Scott Strothmann <smsr3f@mail.missouri.edu>
2016-09-21 00:27:19 +0000

    Add Account Activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'rails-tutorial-smsr3f.c9users.io' # Don't use this literally; use your local dev host instead
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
